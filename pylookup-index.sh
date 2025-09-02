cc2ada74b79592e6e27ec395a8fefdccdd4b3784
Anton Shevchenko <shevaged1990@gmail.com>
2025-09-02 19:48:01 +0000

    Add files via upload

---
+++ pylookup-index.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+./pylookup.py -d $HOME/var/pylookup/pylookup.db \
+  -u /usr/share/doc/python/html/ \
+  -u /home/k/src/django/docs/_build/html \
+  -u ~/src/ansible/docs/docsite/_build/html
+#  -u ~/src/ansible/docs/docsite/_build/html
+#  -u /home/avk/src/wheezy.web/doc/_build
+#  -u ~/src/pymongo/doc/_build/html/
+#  -u ~/docs/pymongo-2.3
